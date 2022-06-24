9e4a4b64739dd14fe9e0a29e2684d50c2bc7b998
raspuna <raspuna@gmail.com>
2022-06-23 17:18:21 -0700

    server.js

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+require("./config/mongoose.config");
+//this is for authentication
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const PORT = 8000;
+app.use(cookieParser());
+// this is for authentication
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+require("./routes/user.routes")(app);
+// add more routes
+//require("./routes/ .routes")(app);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
