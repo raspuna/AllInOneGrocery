10252433a4b0041377695c21eda28b4e5111ee35
Christopher Alphonse <christopheralphonse96.com>
2022-06-25 19:54:38 -0400

    :zap: made some changes

---
+++ server/models/grocery.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const GrocerySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      maxlength: 60,
+    },
+    desc: {
+      type: String,
+      required: true,
+      maxlength: 200,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    prices: {
+      type: [Number],
+      required: true,
+    },
+    extraGroceries: {
+      type: [
+        {
+          text: { type: String, required: true },
+          price: { type: Number, required: true },
+        },
+      ],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Grocery ||
+  mongoose.model("Grocery", ProductSchema);
