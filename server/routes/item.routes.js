ca3676ff35c6ee5e404aeabebfc1cb3aa72755c2
Ryan Meza <ryanmeza116@gmail.com>
2022-06-25 11:14:47 -0400

    item routes

--- server/routes/item.routes.js
+++ server/routes/item.routes.js
@@ -1,1 +1,9 @@
-// routes for items
+const ItemController = require('../controllers/item.controller');
+
+module.exports = (app) => {
+    app.post('api/item', ItemController.createItem);
+    app.get('api/items', ItemController.getAllItems);
+    app.get('api/items/_id',ItemController.getOneItem);
+    app.put('api/items/:id', ItemController.updateItem);
+    app.delete('api/item/:id', ItemController.deleteItem);
+}
