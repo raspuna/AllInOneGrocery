403541eb8331a14b01dbc43d20d2edb1de14adc9
Ryan Meza <ryanmeza116@gmail.com>
2022-06-27 19:07:36 -0400

    before start

--- client/src/components/items/addGrocery.js
+++ client/src/components/items/addGrocery.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import axios from "axios"; 
-import {Link, Navigate, useNavigate} from "react-router-dom";
+import {Link, navigate, useNavigate} from "react-router-dom";
 
 function CreateProduct (props)  {
     const {productList, setProductList} = props; 
@@ -42,8 +42,48 @@ return (
         <input type = "text" value = {itemName} name = "itemName"
         onchange = {(e) => setItemName(e.target.value)} />
         {errors.itemName && <p clasName = "text-danger">{errors.itemName.message}</p>}
+        <br />
         
+        <label>Grocery Class : </label>
+        <select onChange = {(e) => setItemClass(e.target.value)}>
+            <option>Please Select The Item Class :</option>
+            <option value = "Vegetable"></option>
+            <option value = "Fruit"></option>
+            <option value = "Meat"></option>
+            <option value = "Canned Good"></option>
+            <option value = "Dry Food"></option>
+            <option value = "Misc."></option>
+            </select>
+        <br />
+
+        <label> Grocery Quantity : </label>
+        <input type = "number" value = {itemQuantity} name = "itemQuantity"
+        onchange = {(e) => setItemQuantity(e.target.value)} />
+        {errors.itemQuantity && <p className = "text-danger"> {errors.itemQuantity.message}</p>}
+        <br />
+
+        <label>Grocery Description : </label>
+        <textarea id = "itemDescription" name = "itemDescription" rows = "4" cols = "50"
+        onChange = {(e) => setItemDescription(e.target.value)} />
+        {errors.itemDescription && <p className = "text-danger"> {errors.itemDescription.message}</p>}
+        <br />
+
+        <label>Grocery Price : </label>
+        <input type= "number" value = {itemPrice} name = "itemPrice"
+        onChange = {(e) => setItemPrice(e.target.value)} />
+        {errors.itemPrice && <p className = "text-danger"> {errors.itemPrice.message}</p>}
+        <br />
+
+        {/* <label>Grocery Store ID</label>
+        <input type = "number" value = {groceryId} name = "groceryId"
+        onChange = {(e) => setGroceryId(e.target.value)} />
+        {errors.groceryId && <p className = "text-danger"> {errors.groceryId.message}</p>} */}
+        <input type = "submit" value = {props.buttonText} />
+
+
+
 
     </form>
 )
 }
+export default CreateProduct;
