577b6be39b585b28afa9785e110e72c7b59f1af7
raspuna <raspuna@gmail.com>
2022-06-28 17:14:45 -0700

    show store list in Index.js

--- client/src/components/store/OneStore.js
+++ client/src/components/store/OneStore.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-function OneStore () {
-  <div>This will be the component to show one store and it's groceries</div>
-
-};
+function OneStore() {
+  return (
+    <div>This will be the component to show one store and it's groceries</div>
+  );
+}
 
 export default OneStore;
