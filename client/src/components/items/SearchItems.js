60b159bd7b16481b3bc014e086760220d58ee753
raspuna <raspuna@gmail.com>
2022-06-29 23:09:02 -0700

    removed redundant code

--- client/src/components/items/SearchItems.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-
-function SearchItems(props) {
-  const { itemList } = props;
-  return <Container>{itemList}</Container>;
-}
-
-export default SearchItems;
