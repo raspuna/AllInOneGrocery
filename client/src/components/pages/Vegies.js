417427f01b6fb72c801ca14a3a4aab8c1f9ed21c
Christopher Alphonse <christopheralphonse96.com>
2022-06-29 22:57:51 -0400

    veg

---
+++ client/src/components/pages/Vegies.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  Container,
+  FormControl,
+  Nav,
+  Navbar,
+  Form,
+  // Button,
+  Image,
+} from "react-bootstrap";
+
+const Vegies = () => {
+  return <div>Vegies</div>;
+};
+
+export default Vegies;
