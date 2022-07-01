88e25a68a5b3932250d4a15a927d091ce86e0053
Christopher Alphonse <christopheralphonse96.com>
2022-07-01 19:01:20 -0400

    swiper

--- client/src/components/store/DisplayStores.js
+++ client/src/components/store/DisplayStores.js
@@ -1,6 +1,13 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Container, Row, Col } from "react-bootstrap";
+
 // import GoogleMapAPI from "./util/GoogleMapPlace";
 
 function DisplayStores(props) {
@@ -21,17 +28,49 @@ function DisplayStores(props) {
   console.log(storeList);
 
   return (
-    <div>
-      {storeList &&
-        storeList.map((store, index) => (
-          <div key={index}>
-            <p>
-              <Link to={`/stores/${store._id}`}>{store.storeName}</Link>
-            </p>
-            {/* <p>Distance to: Would need the logic for searched location to store distance here?</p> */}
-          </div>
-        ))}
-    </div>
+    <>
+      <Container>
+        <Row>
+          {storeList &&
+            storeList.map((store, index) => (
+              <div key={index}>
+                <p>
+                  <Link to={`/stores/${store._id}`}>{store.storeName}</Link>
+                </p>
+                <p>
+                  Distance to: Would need the logic for searched location to
+                  store distance here?
+                </p>
+              </div>
+            ))}
+
+          <Col>
+            {" "}
+            <Swiper
+              slidesPerView={4}
+              spaceBetween={30}
+              loop={true}
+              pagination={{
+                clickable: true,
+              }}
+              navigation={true}
+              modules={[Pagination, Navigation]}
+              className="mySwiper"
+            >
+              <SwiperSlide>Slide 1</SwiperSlide>
+              <SwiperSlide>Slide 2</SwiperSlide>
+              <SwiperSlide>Slide 3</SwiperSlide>
+              <SwiperSlide>Slide 4</SwiperSlide>
+              <SwiperSlide>Slide 5</SwiperSlide>
+              <SwiperSlide>Slide 6</SwiperSlide>
+              <SwiperSlide>Slide 7</SwiperSlide>
+              <SwiperSlide>Slide 8</SwiperSlide>
+              <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 export default DisplayStores;
